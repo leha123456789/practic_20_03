4a8a32eed91edf6e42567004966bd360ac98f05e
Tengen <leha.denisov.03@mail.ru>
2024-04-03 14:38:52 +0300

    Update practic.cpp

--- practic.cpp
+++ practic.cpp
@@ -3,11 +3,11 @@
 #include <omp.h>
 using namespace std;
 using namespace cv;
-void applyGrayscaleFilter(Mat& img) 
+void applyGrayscaleFilter(Mat& img)
 {
     cvtColor(img, img, COLOR_BGR2GRAY);
 }
-void applySepiaFilter(Mat& img) 
+void applySepiaFilter(Mat& img)
 {
     Mat kernel = (Mat_<float>(3, 3) << 0.272, 0.534, 0.131, 0.349, 0.686, 0.168, 0.393, 0.769, 0.189);
     transform(img, img, kernel);
@@ -16,14 +16,23 @@ void applyNegativeFilter(Mat& img)
 {
     bitwise_not(img, img);
 }
-void applyEdgeFilter(Mat& img) 
+void applySobelFilter(Mat& img)
 {
-    Canny(img, img, 100, 200);
+    Mat sobelX, sobelY;
+    Sobel(img, sobelX, CV_16S, 1, 0);
+    Sobel(img, sobelY, CV_16S, 0, 1);
+    Mat absSobelX, absSobelY;
+    convertScaleAbs(sobelX, absSobelX);
+    convertScaleAbs(sobelY, absSobelY);
+    addWeighted(absSobelX, 0.5, absSobelY, 0.5, 0, img);
 }
-int main() 
+int main()
 {
-  
     Mat image = imread("input.jpg");
+    if (image.empty()) {
+        cerr << "Не удалось загрузить изображение." << endl;
+        return -1;
+    }
     Mat grayscaleImage = image.clone();
     Mat sepiaImage = image.clone();
     Mat negativeImage = image.clone();
@@ -50,8 +59,8 @@ int main()
         }
 #pragma omp section
         {
-            applyGrayscaleFilter(edgeImage); 
-            applyEdgeFilter(edgeImage);
+            applyGrayscaleFilter(edgeImage);
+            applySobelFilter(edgeImage);
             imshow("Контур", edgeImage);
             waitKey(0);
         }
